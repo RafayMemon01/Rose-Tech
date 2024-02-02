e841edb2292087fea230c6f81037fefcf8c415a3
Rafay Memon <rafayrashid200611@gmail.com>
2024-02-02 11:22:06 +0500

    event section done all event s added

--- Events/events.js
+++ Events/events.js
@@ -1,28 +1,92 @@
 const eventsData = {
-    "events": [
-      {
-        "imageurl": "https://lh3.googleusercontent.com/pw/ABLVV87b1D0ZgvLNzhUp04W34D9hr8UYm2UQGgnW_tk2GlFxHlBgwPQ89ybd9x7tqf8zxa9JBFTzKta8Wp8glVSsx_OcoePQfrlBZ33rHCuVfQI1rUKyAHrnqGqWK3djJ7CRPBrhmUTokrzihSXhykbFqg7x=w1196-h897-s-no-gm?authuser=0",
-        "learnmoreandimageanchorlink": "https://lh3.googleusercontent.com/pw/ABLVV87b1D0ZgvLNzhUp04W34D9hr8UYm2UQGgnW_tk2GlFxHlBgwPQ89ybd9x7tqf8zxa9JBFTzKta8Wp8glVSsx_OcoePQfrlBZ33rHCuVfQI1rUKyAHrnqGqWK3djJ7CRPBrhmUTokrzihSXhykbFqg7x=w1196-h897-s-no-gm?authuser=0",
-        "title": "I.T. Awareness Session by Rosetech Team on September 26!",
-        "description": "Dynamic speakers Raza Suriya and Umair Farooq illuminated the minds of aspiring young minds at the Dreams Of Youth Welfare Society, delving into the realms of Freelancing, Video Editing, and Digital Marketing. The enthusiastic participation and avid learning of the students made this session truly exceptional!",
-        "date": "26, Sep,2023"
-      },
-      {
-        "imageurl": "https://lh3.googleusercontent.com/pw/ABLVV84EGb7ozNUYOlXbEiRXaKh0uZDZhk7OQ-RQLj7IrIPmAgUBx9ieAybsdDg-sb5D7CeG7gfcCCGrtT9wSg_KxaoWmwGDwOt0pWJUWK1A2x3ifB0GfgFxFHGaBktfm0qeyF4L7UNeHiVdg_5X8F3K3AlM=w1600-h720-s-no-gm?authuser=0",
-        "learnmoreandimageanchorlink": "https://www.facebook.com/rosetechlyari/posts/pfbid0LsgccYBQaoWSADWc5rHZYbXLhFPwQvPiZh5yvSYUxDHThC1oMe2iRTQtfz5pjBUPl",
-        "title": "Graduation Day for DIT and CIT Courses on 5th August 2023",
-        "description": "Rosetech Computer Institute Commemorates Graduation Day for DIT and CIT Courses on 5th August At Rose Youth Point - Karachi Sindh. Director of Rose Education Society, Anwar Ali Bhatti, and Administrator Najma Jabeen expressed their appreciation for this achievement. Chairman of Lyari, Nasir Kareem Baloch, and D M Danish Baloch Of Administrator Al-Qadir Model English School and Arsalan Amin Assistant Manager UI / UX Development At TechClone PVT also participated in this event. Co-Founder of Rosetech Umair Farooq was acknowledged for his visionary leadership. As we move towards more such invaluable moments.",
-        "date": "Auguest 5, 2023"
-      },
-      {
-        "imageurl": "https://lh3.googleusercontent.com/pw/ABLVV87AYR97uFWVIO5TKGo1_RNH4zeEJodjXLUGWt_lVT00iLzVpBQq3NVXXLqrAr12SMPPhYjlhQyu9fTxKgAfeeGwaiEboWj2eh8CSuI3rNkxJAbC6EzQSgHDGvWXN7RhNf3uDtvQBMujeFlHTmpjjwiB=w1196-h897-s-no-gm?authuser=0",
-        "learnmoreandimageanchorlink": "https://www.facebook.com/rosetechlyari/posts/pfbid0RG2xbnbcbXHhRFbRRaoUous93rZVZN8GB4jVSurdreLgxzZ8R6tVsSSPgUxzFA4Bl",
-        "title": "Engaging session on Video Editing and Freelancing",
-        "description": "40+ enthusiastic young minds gathered at Rose Youth Point for an engaging session on Video Editing and Freelancing, led by the dynamic speaker, Raza Suriya. Their eagerness to learn was palpable!",
-        "date": "17 Sep, 2023"
-      }
-    ]
-  };
-  
- export default eventsData;
-  
+  events: [
+    //event no 1
+    {
+      imageurl:"https://lh3.googleusercontent.com/pw/ABLVV849KNotP2O3XL9yddI4t8Yn9Vu1Xd6O9FZ3idDW7oc5BjF8EljUNZCM4xtqGV_QOrX1UFiMShHkmeM2fcZiE8MYtPYogs4TNS0iHeMBloQN65IvDq-Z_lSRiO78llKju_Gt-xW9f5YfqCaHumkZwDsn=w817-h576-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:"https://www.facebook.com/rosetechlyari/posts/pfbid02AbWEQxBZBemzXpE4dvzyEFSLc5twwuzv6zWc8qCZkUuUHTbtQytnLdy6FphzyS3gl",
+      title: "Graduation Day for DIT and CIT courses",
+      description: "The event was graced by the presence of Anwar Ali Bhatti, Director of Rose Education Society, and Administrator Najma Jabeen, who expressed their commendation for the students' achievements. Special recognition was given to the visionary leadership of Umair Farooq, the Program Director of Rosetech.",
+      date: "January 25, 2024",
+    },
+    //event no 2
+    {
+      imageurl:"https://lh3.googleusercontent.com/pw/ABLVV87fJHL40LatxYcBiG3NpENlAMl7T0rVoNWF2AraHts6s7rNK3Mfk4LBX20B554_mkJjDGDRY1pDLCTk2ZSoOIrDHVWnCaaNxiewD9HfL0kU4K432G-_mG8B5wkSqQc7Q754rf7X4r5YBNvSy2vIrH8M=w960-h720-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:"https://www.facebook.com/rosetechlyari/posts/pfbid02xtsZdazxhjHqNJzKXtdrHC88P8ER6koqdVuhxH3upBwYLXVZJrrQDfYzBL9WfwBgl",
+      title: "Collaboration with Khathiyawar Nasar-pur Memon Jamat",
+      description: "In a recent meeting, Muhammad Ali Memon, Vice President & Chairman of the Education Committee, alongside Muhammad Nasir Younus, a Member of the Education Committee, representing the Jamat, joined hands with Rosetech's Director Anwar Ali Bhatti and Programme Director Umair farooq revealing some exciting developments.",
+      date: "January 22, 2024",
+    },
+    
+    //event no 3
+    {
+      imageurl:
+        "https://lh3.googleusercontent.com/pw/ABLVV87b1D0ZgvLNzhUp04W34D9hr8UYm2UQGgnW_tk2GlFxHlBgwPQ89ybd9x7tqf8zxa9JBFTzKta8Wp8glVSsx_OcoePQfrlBZ33rHCuVfQI1rUKyAHrnqGqWK3djJ7CRPBrhmUTokrzihSXhykbFqg7x=w1196-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:
+        "https://lh3.googleusercontent.com/pw/ABLVV87b1D0ZgvLNzhUp04W34D9hr8UYm2UQGgnW_tk2GlFxHlBgwPQ89ybd9x7tqf8zxa9JBFTzKta8Wp8glVSsx_OcoePQfrlBZ33rHCuVfQI1rUKyAHrnqGqWK3djJ7CRPBrhmUTokrzihSXhykbFqg7x=w1196-h897-s-no-gm?authuser=0",
+      title: "I.T. Awareness Session by Rosetech Team on September 26!",
+      description:
+        "Dynamic speakers Raza Suriya and Umair Farooq illuminated the minds of aspiring young minds at the Dreams Of Youth Welfare Society, delving into the realms of Freelancing, Video Editing, and Digital Marketing. The enthusiastic participation and avid learning of the students made this session truly exceptional!",
+      date: "26, Sep,2023",
+    },
+    //event no 4
+    {
+      imageurl:
+        "https://lh3.googleusercontent.com/pw/ABLVV87AYR97uFWVIO5TKGo1_RNH4zeEJodjXLUGWt_lVT00iLzVpBQq3NVXXLqrAr12SMPPhYjlhQyu9fTxKgAfeeGwaiEboWj2eh8CSuI3rNkxJAbC6EzQSgHDGvWXN7RhNf3uDtvQBMujeFlHTmpjjwiB=w1196-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:
+        "https://www.facebook.com/rosetechlyari/posts/pfbid0RG2xbnbcbXHhRFbRRaoUous93rZVZN8GB4jVSurdreLgxzZ8R6tVsSSPgUxzFA4Bl",
+      title: "Engaging session on Video Editing and Freelancing",
+      description:
+        "40+ enthusiastic young minds gathered at Rose Youth Point for an engaging session on Video Editing and Freelancing, led by the dynamic speaker, Raza Suriya. Their eagerness to learn was palpable!",
+      date: "17 Sep, 2023",
+    },
+    //event no 5
+    {
+      imageurl:
+        "https://lh3.googleusercontent.com/pw/ABLVV84EGb7ozNUYOlXbEiRXaKh0uZDZhk7OQ-RQLj7IrIPmAgUBx9ieAybsdDg-sb5D7CeG7gfcCCGrtT9wSg_KxaoWmwGDwOt0pWJUWK1A2x3ifB0GfgFxFHGaBktfm0qeyF4L7UNeHiVdg_5X8F3K3AlM=w1600-h720-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:
+        "https://www.facebook.com/rosetechlyari/posts/pfbid0LsgccYBQaoWSADWc5rHZYbXLhFPwQvPiZh5yvSYUxDHThC1oMe2iRTQtfz5pjBUPl",
+      title: "Graduation Day for DIT and CIT Courses on 5th August 2023",
+      description:
+        "Rosetech Computer Institute Commemorates Graduation Day for DIT and CIT Courses on 5th August At Rose Youth Point - Karachi Sindh. Director of Rose Education Society, Anwar Ali Bhatti, and Administrator Najma Jabeen expressed their appreciation for this achievement. Chairman of Lyari, Nasir Kareem Baloch, and D M Danish Baloch Of Administrator Al-Qadir Model English School and Arsalan Amin Assistant Manager UI / UX Development At TechClone PVT also participated in this event. Co-Founder of Rosetech Umair Farooq was acknowledged for his visionary leadership. As we move towards more such invaluable moments.",
+      date: "Auguest 5, 2023",
+    },
+    //event no 6
+    {
+      imageurl: "https://lh3.googleusercontent.com/pw/ABLVV87h097Qi9y1iQuK10SH2WezJLifoNz59d_2ddYRs1mtjZHXo3SqPvReM0Huu79B9A6ay-xPcEYZXkvWjpCC3JoZUuV7C5OyWG8N3DCQENiL8tYNIDxOxwQcddC6j8clQxLecjncBcVpvCsgyc4o5fbS=w1196-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink: "https://www.facebook.com/rosetechlyari/posts/pfbid03BmHyuqfKfwWuqfLA3HSxbSiGj1dw42CynPp68Cvp5H8QLsRcghRRQEXRL4TFE3zl",
+      title: "Certificate Distribution for DIT and CIT Courses on 13th March 2023",
+      description: "This joyous occasion recognized the remarkable achievements of students who successfully completed courses in IT Diploma, MS Office, Graphic Designing, and Video Editing Short Courses. As we applaud their dedication and hard work, we wish these bright individuals the very best on their future endeavors. May your journey ahead be filled with triumphs and endless possibilities!"
+      ,date:"13 Mar, 2023"
+    },
+    {
+      imageurl: "https://lh3.googleusercontent.com/pw/ABLVV86zz03gLgZEq4sKzbfAyOpaswBfDYhI8_hDonD0VcGuT1l4vXVppjc8_de4nVlMlcFI9qiDgzjjgbtHe9d2SBKhYc2PqB8ekAO0CaXZUa1TAcNUyaGPzC38xSYXGtxVaGLCy54kuOyuynuzGuOlpgXj=w1196-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink: "https://www.facebook.com/rosetechlyari/posts/pfbid0MZex9x42D1vfKqsDLriG6Nvhf1w14Vjiho4DsRDqNEEgUGXK3pfb2kQbD5hEFRo2l",
+      title: "Domain hosting classes were conducted by Hasnain khan",
+      description: "Domain hosting is like the stage where your website performs, and Hasnain Khan, a skilled Full Stack Developer, recently conducted enlightening classes on this vital aspect. In these sessions, participants learned the art of creating their inaugural websites and the subsequent process of seamlessly connecting them to domain hosting. Hasnain Khan's expertise illuminated the path, making the journey from website creation to hosting a domain a comprehensible and empowering experience."
+      ,date:"03 Mar, 2023"
+    },
+    {
+      imageurl: "https://lh3.googleusercontent.com/pw/ABLVV87Dy8lmAB6I5BE3R3dJJuG-MPHi-EVrOAenoSKtSvwzPcUWzms3nysMOCmlS-yCUcfgn-MyKzL5QNgkOI1KfzFuiUJJOuPPzF356Mu5v1BPlx4GpFTHgpVmtd26Hig090o1-655T23tE3FxViHptDEV=w1196-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink: "https://www.facebook.com/rosetechlyari/posts/pfbid02q1HKgQfGqQMfgyVv4FZLmknqqXmqpQKerXuCKXDmmHDi2aTGiFywo2sJEcFN5BJFl",
+      title: "Interesting Training session with the youth of Rose tech at Rose youth point",
+      description: "Rose Tech's youth had an enriching training session led by Hamza Junani, covering startups, L&D, HR, and tech. Umair Farooq and Khalid Ahmed Baloch, Rose Tech leaders, managed the session. Special thanks to Sir Anwar Ali Bhatti. Hamza introduced his new course, L&OD Practice with Hamza Junani, garnering appreciation. The youth expressed gratitude to Hamza and Rose Tech for the valuable session."
+      ,date:"07 Jan, 2023"
+    },
+    {
+      imageurl: "https://lh3.googleusercontent.com/pw/ABLVV86e9sPL9T1T-D-MnAmQiI0dIglghA_vYlGwTYQYWZBaxo45ukJeiDTLpVH-E_6iZIhlLSH1JPT-TpNLDkEHRaDbr-Pi4Y-bLGfEnur22D5zHCuu37VWhCqdTleyRvlB7EfGqXVsuerlkUD_M7MpY5GK=w960-h540-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink: "https://www.facebook.com/rosetechlyari/posts/pfbid0J4rPiq9e4h5GQSPmCg5XABc2QvkJJWRkd4oTcMEfPVUywBTLrdYLYLmL4hzZhevkl",
+      title: "Certificate Distribution for DIT and CIT Courses on 30th Augest 2022",
+      description: "Rosetech Computer Institute joyfully distributed certificates to students who successfully completed IT Diploma, MS Office, and Graphic Designing Short Course. Wishing the graduates all the best on their future endeavors! "
+      ,date:"07 Jan, 2023"
+    },
+    {
+      imageurl:"https://lh3.googleusercontent.com/pw/ABLVV87xWp6d2NVRBZnjvTKMmYfhu3vEM7Mjg_0z5h_sPH4bOvMMdHa6Xhdi1A8CiMXoJ83K4AbWJ5k4nKDZdjCUXgeGfBzkr3M9fhpGQGXrBJCK7CZJQTocm8auTbiyH85EAMdCENbUimVcWerQvf2HT6WZ=w1346-h897-s-no-gm?authuser=0",
+      learnmoreandimageanchorlink:"https://www.facebook.com/worldsoftech/photos/a.1089544201392899/1089656898048296/?type=3",
+      title: "",
+      description: "",
+      date: "Jan 13, 2020",
+    },
+  ],
+};
+
+export default eventsData;
